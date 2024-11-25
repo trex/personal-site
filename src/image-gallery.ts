68f9a097e801d712bf2eea46afb66a983303d121
Trevor Smith <trevorsmith@Trevors-Air.home>
2024-11-24 21:55:29 -0700

    Image attributes on art gallery items

---
+++ src/image-gallery.ts
@@ -0,0 +1,115 @@
+import bagAnimation from '/art/bag-animation.gif';
+import blueOysters from '/art/blue-oysters.jpg';
+import boletePetey from '/art/bolete-petey.jpg';
+import cantWearNikes from '/art/cant-wear-nikes.jpg';
+import moundToTheWind from '/art/mound-to-the-wind.png';
+import mushroomBuds from '/art/mushroom-buds.png';
+import newShit from '/art/new-shit.jpg';
+import ousideJulesCover from '/art/outside-jules-cover.png';
+import outsideJulesSleeping from '/art/outside-jules-sleeping.png';
+import spinningHeads from '/art/spinning-heads.jpg';
+import spiritOfTheTree from '/art/spirit-of-the-tree.jpg';
+import totoroMural from '/art/totoro-mural.png';
+import yeehawPadnah from '/art/yeehaw-padnah.png'
+import yieldSign from '/art/yield.jpg';
+
+export const images = [
+    {
+        src: ousideJulesCover,
+        title: "Outside Jules Cover",
+        year: 2024,
+        medium: "Pen & Ink, Digitally Colored",
+        statement: "The cover for Outside Jules, a comic I write and illustrate."
+    },
+    {
+        src: cantWearNikes,
+        title: "I Can't Wear Nikes",
+        year: 2014,
+        medium: "Ink & Watercolor",
+        statement: "Illustration of a moment and quote from my 4y.o. son."
+    },
+    {
+        src: boletePetey,
+        title: "Bolete Petey",
+        year: 2024,
+        medium: "Pen & Ink, Digitally Colored",
+        statement: "Main character from We Happen to be Mushrooms, a comic strip I write and Illustrate."
+    },
+    {
+        src: spiritOfTheTree,
+        title: "Spirit of the Tree",
+        year: 2022,
+        medium: "Watercolor",
+        statement: "Sometimes the trees talk to me. Sometimes I listen. Sometimes I talk back."
+    },
+    {
+        src: newShit,
+        title: "Time for Some New Shit",
+        year: 2009,
+        medium: "Marker",
+        statement: "I like drawing creatures with eyes."
+    },
+    {
+        src: mushroomBuds,
+        title: "We Happen to be Mushrooms",
+        year: 2024,
+        medium: "Pen & Ink, Digitally Colored",
+        statement: "These three mushroom buds are the main characters of We Happen to be Mushrooms, a little comic strip."
+    },
+    {
+        src: totoroMural,
+        title: "Totoro Mural",
+        year: 2022,
+        medium: "Acrylic paints",
+        statement: "My Neighbor Totoro mural that I may or may not ever finish."
+    },
+    {
+        src: yieldSign,
+        title: "Yield",
+        year: 2015,
+        medium: "Concrete, wood, old computers and wires and shit",
+        statement: "A physical manifestation of my dynamic and varied feelings about and relationship with technology."
+    },
+    {
+        src: outsideJulesSleeping,
+        title: "Jules Waking",
+        year: 2024,
+        medium: "Pen & Ink, Digitally Colored",
+        statement: "Opening sequence from Outside Jules."
+    },
+    {
+        src: blueOysters,
+        title: "Blue Oysters",
+        year: 2020,
+        medium: "Watercolor",
+        statement: "Blue oyster mushrooms I painted for my mom, and then she gave back to me."
+    },
+    {
+        src: bagAnimation,
+        title: "Critter Bag Animation",
+        year: 2016,
+        medium: "Colored Pencil, Stop-Motion Animation",
+        statement: "Animation I created for a brownbag lunch lesson I gave at SendGrid on stop-motion animation."
+    },
+    {
+        src: moundToTheWind,
+        title: "Mound to the Wind",
+        year: 2017,
+        medium: "Pen & Ink, Watercolor",
+        statement: "Not much to say about this one."
+    },
+    {
+        src: spinningHeads,
+        title: "Spinning Heads",
+        year: 2022,
+        medium: "Watercolor",
+        statement: "Sometimes my head feels a little spinny like."
+    },
+    {
+        src: yeehawPadnah,
+        title: "Yeehaw Padnah",
+        year: 2024,
+        medium: "Pen & Ink, Digitally Colored",
+        statement: "Video game loading screen from Outside Jules."
+    }
+];
