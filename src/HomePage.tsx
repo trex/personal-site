8bafab668b14a3c9206ff629baa41b3c68ddd38a
Trevor Smith <trevorsmith@Trevors-Air.home>
2024-12-02 12:04:04 -0700

    Homepage image update, expand blurb

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,10 +1,10 @@
 import ContactForm from './ContactForm';
-import bagAnimation from '/art/bag-animation.gif';
+import elBarto from '/el-barto.jpg';
 
 function HomePage() {
     return <>
-        <p>t sure does like making things</p>
-        <img className="splash-image" src={bagAnimation} alt="Bag Animation" />
+        <img className="splash-image" src={elBarto} alt="Me as el Barto" />
+        <p className="homepage-blurb">Hi, I’m T – a maker of many things! Whether it’s designing intuitive tech solutions or crafting vibrant artwork, I thrive at the intersection of creativity and innovation. Take a peek at my project portfolio to see where ideas come to life (and sometimes get a little weird). Let’s make something amazing together!</p>
         <ContactForm />
     </>
 }
